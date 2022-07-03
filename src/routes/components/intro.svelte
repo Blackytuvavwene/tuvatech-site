43b6ec34e1b13453d5d6f56f0d082de6fa6f15de
Boitumelo Tubabwene <btexblacky@gmail.com>
2022-07-03 22:55:03 +0200

    changed launch date

--- src/routes/components/intro.svelte
+++ src/routes/components/intro.svelte
@@ -1,5 +1,5 @@
 
 <h1 class="text-4xl font-extrabold text-center m-4">Coming Soon</h1>
-<p class="text-2xl text-center m-2">The new TuvaTech website will be launching soon on June 30, 2022</p>
+<p class="text-2xl text-center m-2">The new TuvaTech website will be launching soon on August 21, 2022</p>
 
 
